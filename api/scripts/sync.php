117da5e75198504883d4206112eb1a8a74638c4a
Fernando Bevilacqua <dovyski@gmail.com>
2012-01-17 18:29:51 -0200

    API: working sync script.
    * It's working but is not optimized and has some known bugs.

--- api/scripts/sync.php
+++ api/scripts/sync.php
@@ -10,6 +10,8 @@
  * This script is supposed to be run using a command line.
  */
 
+require_once dirname(__FILE__).'/../Db.php';
+
 // TODO: use an INI file or something
 define('WP_PREFIX', 'wp_');
 
@@ -18,16 +20,34 @@ define('WP_PREFIX', 'wp_');
  * 
  * @param string $theSql the SQL query.
  */
-function dbQuery($theSql) {
-	static $sConnection;
+function dbQuery($theSql, $theConnectionId = 0) {
+	static $sConnection = array();
 	
-	if(!isset($sConnection)) {
+	if(!isset($sConnection[$theConnectionId])) {
 		// TODO: use an INI file or something
-		$sConnection = mysql_connect("localhost", "root", "") or die("Unable to connect to database");
-		mysql_select_db("wordpress") or die("Unable to select database");
+		$aHost = "";
+		$aUser = "";
+		$aPass = "";
+		$aDb   = "";
+		
+		if($theConnectionId == 0) {
+			$aHost = "localhost";
+			$aUser = "root";
+			$aPass = "";
+			$aDb   = "wordpress";
+						
+		} else if($theConnectionId == 1) {
+			$aHost = "localhost";
+			$aUser = "root";
+			$aPass = "";
+			$aDb   = "api_as3gamegears";
+		}
+		
+		$sConnection[$theConnectionId] = mysql_connect($aHost, $aUser, $aPass, true) or die("Unable to connect to database");
+		mysql_select_db($aDb) or die("Unable to select database");
 	}
-	
-	$aRet = mysql_query($theSql, $sConnection) or die("SQL error: " . mysql_error());
+
+	$aRet = mysql_query($theSql, $sConnection[$theConnectionId]) or die("SQL error: " . mysql_error($sConnection[$theConnectionId]));
 	return $aRet;
 }
 
@@ -47,7 +67,7 @@ function getTerm($theTermId, $theColumn = 'slug') {
 }
 
 function findCategories() {
-	$aRes 	= dbQuery("SELECT t.term_id AS id, t.name, t.slug, tt.description FROM ".WP_PREFIX."term_taxonomy AS tt JOIN ".WP_PREFIX."terms AS t ON tt.term_id = t.term_id WHERE tt.taxonomy = 'category' AND t.slug NOT IN ('blog', 'blogroll')");
+	$aRes 	= dbQuery("SELECT tt.term_taxonomy_id AS id, t.name, t.slug, tt.description, tt.parent FROM ".WP_PREFIX."term_taxonomy AS tt JOIN ".WP_PREFIX."terms AS t ON tt.term_id = t.term_id OR tt.parent = tt.term_taxonomy_id  WHERE tt.taxonomy = 'category' AND t.slug NOT IN ('blog', 'blogroll')");
 	$aCat	= array();
 	
 	if(mysql_num_rows($aRes) > 0) {
@@ -74,9 +94,29 @@ function findLicenses() {
 	return $aRet;
 }
 
+/**
+ * Inserts all collected item into the database. This method organizes the item's properties
+ * in a way they can be easily searched in a database query.
+ * 
+ * @param array $theItem associative array describing an item.
+ * @param array $theCategories array of existing categories, indexed by id.
+ * @param arra $theLicenses array of existing licenses, indexed by slug.
+ */
+function insetIntoDb($theItem, $theCategories, $theLicenses) {
+	$aCategory 	= $theItem['category'][0];
+	$aCategory2 = $theCategories[$aCategory]['parent'] != 0 ? $theCategories[$aCategory]['parent'] : 'NULL';
+	$aLicense	= isset($theItem['license'][0]) ? $theItem['license'][0] : 'NULL'; 
+	$aLicense2 	= isset($theItem['license'][1]) ? $theItem['license'][1] : 'NULL';
+	
+	dbQuery("INSERT IGNORE INTO ".Db::TABLE_ITEMS." (id, name, description, excerpt, category, category2, license, license2) VALUES (id, '".addslashes($theItem['name'])."', '".addslashes($theItem['description'])."', '".addslashes($theItem['excerpt'])."', ".$aCategory.", ".$aCategory2.", ".$aLicense.", ".$aLicense2.")", 1);
+}
+
 $aResult = dbQuery("SELECT ID, post_content, post_title, post_name FROM ".WP_PREFIX."posts WHERE post_status = 'publish' AND post_type = 'post'");
 
 if(mysql_num_rows($aResult) > 0) {
+	$aCategories = findCategories();
+	$aLicenses	 = findLicenses();
+	
 	while($aItem = mysql_fetch_assoc($aResult)) {
 		$aData = array();
 		
@@ -117,10 +157,11 @@ if(mysql_num_rows($aResult) > 0) {
 				while($aTemp = mysql_fetch_assoc($aResTaxonomies)) {
 					//var_dump($aTemp);
 					if($aTemp['taxonomy'] == 'category') {
-						$aData['category'][] = getTerm($aTemp['term_id']);
+						// TODO: filter unwanted categories such as blog.
+						$aData['category'][] = $aTemp['term_taxonomy_id'];
 						
 					} else if($aTemp['taxonomy'] == 'post_tag') {
-						$aData['license'][] = getTerm($aTemp['term_id']);						
+						$aData['license'][] = $aTemp['term_taxonomy_id'];
 					}
 					
 					//echo "term_id = " . $aTemp['term_id'] . " = ".getTerm($aTemp['term_id'])." (tax ".$aTemp['taxonomy'].")\n";
@@ -130,13 +171,13 @@ if(mysql_num_rows($aResult) > 0) {
 			mysql_free_result($aResTaxonomies);			
 		}
 		
-		
-		var_dump($aData);
+		//var_dump($aData);
+		insetIntoDb($aData, $aCategories, $aLicenses);
 		unset($aData);
 		
 		echo "[OK]\n";
-
 	}
+	echo "All done!\n";
 }	
 
 
