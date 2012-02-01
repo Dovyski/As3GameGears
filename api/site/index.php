c05f853b7ae9e9c54a366ae4d92d47776461e294
Fernando Bevilacqua <dovyski@gmail.com>
2012-02-01 17:59:47 -0200

    API: a little bit of love and content to the API usage site.

--- api/site/index.php
+++ api/site/index.php
@@ -3,9 +3,9 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Kingade - game development to teach kids about environment care!</title>
-    <meta name="title" content="Kingade">    
-    <meta name="description" content="Game development to teach kids about environment care!">
+    <title>As3GameGears API Usage</title>
+    <meta name="title" content="As3GameGears API Usage">    
+    <meta name="description" content="Usage information for As3GameGears API">
     <meta name="author" content="Fernando Bevilacqua">
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
@@ -14,7 +14,7 @@
     <![endif]-->
     
     <!-- Le SEO -->
-    <link rel="canonical" href="http://kingade.com/" />
+    <link rel="canonical" href="http://api.as3gamegears.com/1.0/usage/" />
 
     <!-- Le styles -->
 	<link href='http://fonts.googleapis.com/css?family=Cabin+Sketch' rel='stylesheet' type='text/css'>
@@ -23,15 +23,15 @@
 
     <!-- Le fav and touch icons -->
     <link rel="shortcut icon" type="image/x-icon" href="img/ico/favicon.ico">
-    <link rel="apple-touch-icon" href="img/ico/waker-apple-57x57.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="img/ico/waker-apple-72x72.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="img/ico/waker-apple-114x114.png">
+    <link rel="apple-touch-icon" href="img/ico/agg-apple-57x57.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/ico/agg-apple-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/ico/agg-apple-114x114.png">
     
 	<script type="text/javascript">
 	
 	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-10567379-5']);
-	  _gaq.push(['_trackPageview']);
+	  //_gaq.push(['_setAccount', 'UA-10567379-5']);
+	  //_gaq.push(['_trackPageview']);
 	
 	  (function() {
 	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
@@ -40,141 +40,103 @@
 	  })();
 	
 	</script>
-	<script src="https://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js" type="text/javascript"></script>
   </head>
 
   <body>
-	<a href="http://github.com/Dovyski/Kingade" target="_blank"><img style="position: absolute; top: 0; right: 0; border: 0;" src="http://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub" /></a>
+	<a href="http://github.com/Dovyski/As3GameGears" target="_blank"><img style="position: absolute; top: 0; right: 0; border: 0;" src="http://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub" /></a>
 	
     <!-- Masthead (blueprinty thing)
     ================================================== -->
     <header class="jumbotron masthead" id="overview">
       <div class="inner">
         <div class="container">
-          <h1>Kingade</h1>
-          <p>Game development to teach kids about environment care!</p>
+          <a href="/"><img src="img/header-image-simple.png" title="As3GameGears" border="0" /></a>
         </div><!-- /container -->
       </div>
     </header>
 
+	<?php 
+		$aVersion 	= isset($_GET['version']) ? $_GET['version'] : 0; 
+		$aMethod 	= isset($_GET['method']) ? $_GET['method'] : '';
+		
+		if(!empty($aMethod)) {
+	?>
+	
+		<div class="container">
+			<section id="about">
+				<div class="page-header">
+					<h1>Usage of <?php echo $aMethod;?></h1>
+				</div>
+				<div class="row">
+					<div class="span16">
+						<p>This is the As3GameGears API version <code>1.0</code>. Below you can find a list of all available methods. The term <code>slug</code> refers to the "non-spaced" version of a name. E.g. a category named <code>Augmented Reality</code> has as its slug <code>augmented-reality</code>. Every item, category and license on the site are indexed by their slug.</p>
+						<p>The API accepts different return types, such as <code>JSON</code> and <code>XML</code>. In order to specify a return type, use the syntax <code>/method.RETURN_TYPE/</code>. If no return type is specified, the default is <code>JSON</code>. E.g: <br /><br />
+						<code>http://api.as3gamegears.com/1.0/items.xml</code><br />
+						<code>http://api.as3gamegears.com/1.0/items.json</code><br />
+						<code>http://api.as3gamegears.com/1.0/items</code> (defaults to <code>JSON</code>)
+						</p>
+					</div>
+				</div><!-- /row -->
+			</section>
+		</div><!-- /container -->
+	
+	<?php
+		} else {
+	?>
+	
     <div class="container">
 		<section id="about">
-		   <div class="span16 social">
-		   	 <g:plusone size="medium" href="http://kingade.com"></g:plusone>
+		  <div class="page-header">
+		    <h1>About <small>Some words about the API</small></h1>
+		  </div>
+		  <div class="row">
+		    <div class="span16">
+		      <p>This is the As3GameGears API version <code>1.0</code>. Below you can find a list of all available methods. The term <code>slug</code> refers to the "non-spaced" version of a name. E.g. a category named <code>Augmented Reality</code> has as its slug <code>augmented-reality</code>. Every item, category and license on the site are indexed by their slug.</p>
+		      <p>The API accepts different return types, such as <code>JSON</code> and <code>XML</code>. In order to specify a return type, use the syntax <code>/method.RETURN_TYPE/</code>. If no return type is specified, the default is <code>JSON</code>. E.g: <br /><br />
+		      	<code>http://api.as3gamegears.com/1.0/items.xml</code><br />
+		      	<code>http://api.as3gamegears.com/1.0/items.json</code><br />
+		      	<code>http://api.as3gamegears.com/1.0/items</code> (defaults to <code>JSON</code>)
+		      </p>
+		    </div>
+		  </div><!-- /row -->
+		</section>
+		
+		<section id="methods">
+		  <div class="page-header">
+		    <h1>Methods <small>Usage information for all available methods</small></h1>
+		  </div>
+		  <div class="row">
+		    <div class="span15">
+		      This is the list of available methods. Click on any method to get further information such as params and return structure.<br /><br />
+		      <h2>Items</h2>
+		      <p class="method-link"><a href="./items" target="_blank">/items</a> - list all items.</p>
+		      <p class="method-link"><a href="./items" target="_blank">/items/{category-slug}</a> - list the items of a specific category.</p>
+		      <p class="method-link"><a href="./item" target="_blank">/item</a> - get information about a specific item.</p>
+		      
+		      <h2>Search</h2>
+			  <p class="method-link"><a href="./search" target="_blank">/search</a> - search for items using an arbitrary text query.</p>
 				
-			  <a href="https://twitter.com/share" class="twitter-share-button" data-via="as3gamegears" data-related="as3gamegears">Tweet</a>
-			  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-	      </div>
-	      
-		  <div class="page-header">
-		    <h1>About <small>How can we change the world?</small></h1>
-		  </div>
-		  <div class="row">
-		    <div class="span15">
-		      If you want to change the world, start by teaching new ideas and values to kids. They will be tomorrow's citizens and will live according to what they learned from their parents, friends and school. What can we do to teach kids about environment care? We use something they like a lot: <strong>games</strong>!<br /><br />
-		    </div>
-		    <div class="span16">
-		      <img src="img/math.png" alt="Kids + Game Development = Better World!" />
-		    </div>
-		    <div class="span-one-third centered">
-		      <h2>Kids</h2><br />
-			  <p>A child that protects the environment today will become an adult that protects the planet tomorrow.</p>
-		    </div>
-		    <div class="span-one-third centered">
-		      <h2>Games</h2><br />
-		      <p>Game development can be used to entertain, but it can also be used to teach great values.</p>
-		    </div>
-		    <div class="span-one-third centered">
-		      <h2>Better World</h2><br />
-		      <p>Imagine a place where nature and humankind can walk side by side, protecting each other.</p>
-		    </div>
-		  </div><!-- /row -->
-		</section>
-		
-		<section id="how">
-		  <div class="page-header">
-		    <h1>How <small>Our idea to teach about environment care</small></h1>
-		  </div>
-		  <div class="row">
-		    <div class="span15">
-		      <img src="img/small_planet_people.png" alt="We can change the world!" style="float: right;" />
-		      This project was born as an idea to share the university knowledge with its nearby community. Using the technology of Computer Science course, the teaching skills of Pedagogy course and the drawings made by kids of public schools, we could produce educational materials to teach about environment care. The plan we executed was the folowing:<br /><br />
-		      <ul>
-		      	<li>Pedagogy students created small workshops focusing on subjects related to environment care, such as saving water, don't waste food, etc. The students were mentored by Pedagogy professors.</li>
-				<li>The workshops were applied to kids from public schools. During the workshops the kids watched videos and discussed about the main topic of every workshop.</li>
-				<li>In the end of each workshop, the kids were asked to write and draw about it. We told them all the drawings would be used to create a computer game.</li>
-				<li>After we transformed all the drawings and some texts from the workshops into game assets. Those assets were delivered to Computer Science students. They used those assets and the Flash platform to create an educational game.</li>
-				<li>The result of this project was a computer game made by students and kids that can be freely used by anyone to teach about environment care.</li>
-		      </ul>  
-		    </div>
-		  </div><!-- /row -->
-		</section>
-		
-		<section id="result">
-		  <div class="page-header">
-		    <h1>Result <small>The kids final product</small></h1>
-		  </div>
-		  <div class="row">
-		    <div class="span15">
-		      The result of this project was a computer game developed in <a href="http://www.adobe.com/products/flashplayer.html" target="_blank">Flash</a> using <a href="http://flixel.org" target="_blank">Flixel</a> and <a href="http://www.photonstorm.com/flixel-power-tools" target="_blank">Flixel Power Tools</a>. You can play it below!<br /><br />   
-		    </div>
-		    
-			<div class="span15" style="text-align: center !important;">
-				<div id="altContent">
-					<p>You need Flash Player to play this game.<a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif"	alt="Get Adobe Flash player" /></a></p>
-				</div>
-			
-				<script type="text/javascript">
-					var flashvars = {
-						lang: "en"
-					};
-					var params = {
-						menu: "false",
-						scale: "noScale",
-						allowFullscreen: "true",
-						allowScriptAccess: "always",
-						bgcolor: "#FFFFFF"
-					};
-					var attributes = {
-						id:"WaterSaverTheMap"
-					};
-					swfobject.embedSWF("WaterSaver.swf", "altContent", "640", "480", "10.0.0", "expressInstall.swf", flashvars, params, attributes);
-				</script>
-			</div>
-		  </div><!-- /row -->
-		</section>
-		
-		<section id="acknowledge">
-		  <div class="page-header">
-		    <h1>Acknowledge</h1>
-		  </div>
-		  <div class="row">
-		    <div class="span16">
-		      <img src="img/testimonial.png" alt="Acknowledge" style="float: left; margin-right: 30px;" />
-		      <p>This project took place in Chapecó, Santa Catarina, Brazil. We would like to thank <a href="http://www.uffs.edu.br" target="_blank">Federal University of Fronteira Sul</a> (UFFS) that supported the project.<br /><br />
-		      We also want to thank the staff and kids from <strong>Lourdes Tonin public school</strong>, the place where we conducted all workshops.<br /><br />
-		      A special thanks to <a href="https://twitter.com/#!/photonstorm" target="_blank">Richard "@photonstorm" Davey</a> for Flixel Power Tools and <a href="http://www.photonstorm.com/archives/2247/flash-game-dev-tip-12-building-a-retro-platform-game-in-flixel-part-1" target="_blank">this tutorial</a>, they really helped all Computer Science students to develop the resulting game. A special thanks to <a href="http://www.trilhasonora.com/" target="_blank">Thiago Gobet Spada</a> for providing us with two musics to be used in the game, for free. Thanks to <a href="http://www.flashkit.com/soundfx/" target="_blank">Sound FX (FlashKit)</a> for all the free sound effects we used in the game.</p>
+		      <h2>Extra</h2>
+			  <p class="method-link"><a href="./categories" target="_blank">/categories</a> - list all categories.</p>
+			  <p class="method-link"><a href="./categories" target="_blank">/categories/{slug}</a> - get information about a specific category.</p>
+			  <p class="method-link"><a href="./licenses" target="_blank">/licenses</a> - list the licenses used by all items.</p>
+			  <p class="method-link"><a href="./licenses" target="_blank">/licenses/{slug}</a> - get information about a specific license.</p>
 		    </div>
 		  </div><!-- /row -->
 		</section>
     </div><!-- /container -->
+    
+	<?php
+		}
+	?>    
 
     <footer class="footer">
       <div class="container">
         <p class="pull-right"><a href="#">Back to top</a></p>
         <p>
-          Project managed by <a href="http://twitter.com/as3gamegears" target="_blank">Fernando Bevilacqua</a> <small>Computer Science Professor, UFFS - fernando.bevilacqua@uffs.edu.br</small><br />
-          All drawings in this page and in the game were created by students from Lourdes Tonin public school.<br />
+          Visit <a href="http://as3gamegears.com" target="_blank">As3GameGears.com</a> - Boost your tools!
         </p>
       </div>
     </footer>
-    
-	<script type="text/javascript">
-	  (function() {
-	    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-	    po.src = 'https://apis.google.com/js/plusone.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-	  })();
-	</script>
   </body>
 </html>
