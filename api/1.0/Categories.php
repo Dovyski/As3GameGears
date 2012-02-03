c4cb66abd66affec32a7349e67761b172c3471f9
Fernando Bevilacqua <dovyski@gmail.com>
2012-02-03 16:26:01 -0200

    API: refactoring categories() and licenses(). No item listing anymore.

--- api/1.0/Categories.php
+++ api/1.0/Categories.php
@@ -2,10 +2,9 @@
 
 /**
  * Display information about categories and their items.
- *
  */
 class Categories {
-	public function index($slug="") {
+	public function index($slug = "") {
 		$aRet = "";
 
 		if(empty($slug)) {
@@ -13,22 +12,11 @@ class Categories {
 			$aRet = Db::findCategories();
 			
 		} else {
-			// Display items from a specific category.
-			$aCategory = Db::getCategoryBySlug($slug);
+			// Display an specific category
+			$aRet = Db::getCategoryBySlug($slug);
 
-			if($aCategory == null) {
+			if($aRet == null) {
 				throw new RestException(400, "Unknown category " . $slug);
-			}
-			
-			$aRet				= new stdClass();
-			$aRet->id			= (int)$aCategory['id'];
-			$aRet->name			= $aCategory['name'];
-			$aRet->slug			= $aCategory['slug'];
-			$aRet->description	= $aCategory['description'];
-			$aRet->items		= array();
-
-			foreach(Db::findItemsByCategoryId($aCategory['id']) as $aKey => $aInfo) {
-				$aRet->items[] = Utils::createItem($aInfo);
 			}
 		}
 
--- api/1.0/Licenses.php
+++ api/1.0/Licenses.php
@@ -4,8 +4,6 @@
  * Display information about licenses and items.
  */
 class Licenses {
-	const INVALID_LICENSE	= 7002;
-	
 	public function index($slug="") {
 		$aRet = "";
 
@@ -14,21 +12,11 @@ class Licenses {
 			$aRet = Db::findLicenses();
 			
 		} else {
-			// Display items using a specific license.
-			$aLicense = Db::getLicenseBySlug($slug);
+			// Display an specific license
+			$aRet = Db::getLicenseBySlug($slug);
 
-			if($aLicense == null) {
-				throw new RestException(self::INVALID_LICENSE, "Unknown license " . $slug);
-			}
-			
-			$aRet				= new stdClass();
-			$aRet->id			= (int)$aLicense['id'];
-			$aRet->name			= $aLicense['name'];
-			$aRet->slug			= $aLicense['slug'];
-			$aRet->items		= array();
-
-			foreach(Db::findItemsByLicenseId($aLicense['id']) as $aKey => $aInfo) {
-				$aRet->items[] = Utils::createItem($aInfo);
+			if($aRet == null) {
+				throw new RestException(400, "Unknown license " . $slug);
 			}
 		}
 
